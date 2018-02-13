670adb465bbc2b335389adb95e79b328955f742b
Gautham Narayan <gsnarayan@gmail.com>
2018-02-13 14:56:51 -0500

    start making this a proper module

---
+++ plasticc/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+'''
+plasticc: Machine Learning for the Photometric LSST Astronomical Time series Classification Challenge
+'''
+
+from __future__ import unicode_literals
+__author__     = 'Gautham Narayan and Daniel Muthukrishna'
+__email__      = 'gsnarayan@gmail.com'
+__maintainer__ = 'Gautham Narayan'
+__license__    = 'MIT'
+__version__    = '0.1'
+__status__ = 'Alpha'
