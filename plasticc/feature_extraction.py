2d9515445438ac4a1e6a653a628a29953adccf07
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2018-03-05 16:02:15 -0500

    Updated to write full mysql features table.

--- plasticc/feature_extraction.py
+++ plasticc/feature_extraction.py
@@ -2,7 +2,6 @@ import os
 ROOT_DIR = os.getenv('PLASTICC_DIR')
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from astropy.stats import sigma_clip
 import astropy.table as at
 import pymysql
@@ -11,17 +10,19 @@ from plasticc.get_data import GetData
 from ANTARES_object.LAobject import LAobject
 from . import database
 
+DIRNAMES = 1
+
 
 def get_light_curves(data_release, field_in='%', sntype_in='%', snid_in='%', limit=None, shuffle=False):
     getdata = GetData(data_release)
     result = getdata.get_lcs_data(columns=['objid', 'ptrobs_min', 'ptrobs_max', 'peakmjd', 'sim_redshift_host'], field=field_in,
                                   sntype=sntype_in, snid=snid_in, limit=limit, shuffle=shuffle, sort=False)
 
-    sntypes_map = getdata.get_sntypes()
-    sntype_name = sntypes_map[sntype_in]
-
-    non_transients = ['RRLyrae', 'Mdwarf', 'Mira']
-    periodic = True if sntype_name in non_transients else False
+    # sntypes_map = getdata.get_sntypes()
+    # sntype_name = sntypes_map[sntype_in]
+    #
+    # non_transients = ['RRLyrae', 'Mdwarf', 'Mira']
+    # periodic = True if sntype_name in non_transients else False
 
     for head, phot in result:
         objid, ptrobs_min, ptrobs_max, peak_mjd, redshift = head
@@ -48,17 +49,15 @@ def get_light_curves(data_release, field_in='%', sntype_in='%', snid_in='%', lim
             yield t, flux, fluxerr, zeropt[0], mjd, flt, objid, sntype_in, redshift
 
 
-def save_antares_features():
+def save_antares_features(data_release, redo=False):
     """
     Get antares object features.
     Return as a DataFrame with columns being the features, and rows being the objid&passband
     """
-    fig_dir = os.path.join(ROOT_DIR, 'plasticc', 'Figures')
-    data_release = '20180221'
+
     field = 'DDF'
-    sntype = 1
+    sntype = '1'
     features = {}
-    redo = True
     table_exists = False
     nrows = 0
     features_out = []
@@ -70,7 +69,7 @@ def save_antares_features():
         t, flux, fluxerr, zeropt, mjd, flt, objid, sntype, redshift = lcinfo
         p = flt[0]
         objid = objid + '_' + p
-        features[objid] = OrderedDict()
+        features = OrderedDict()
 
         try:
             laobject = LAobject(locusId=objid, objectId=objid, time=t, flux=flux, fluxErr=fluxerr, obsId=mjd, passband=flt,
@@ -80,46 +79,46 @@ def save_antares_features():
             continue
 
         # Get Features
-        features[objid]['objid'] = objid
-        features[objid]['sntype'] = sntype
-        features[objid]['redshift'] = redshift
+        features['objid'] = objid
+        features['sntype'] = sntype
+        features['redshift'] = redshift
         stats = laobject.get_stats()[p]
-        features[objid]['nobs'] = stats.nobs
+        features['nobs'] = stats.nobs
         if stats.nobs <= 3:  # Don't store features of light curves with less than 3 points
             continue
-        features[objid]['variance'] = stats.variance
-        features[objid]['skewness'] = stats.skewness
-        features[objid]['kurtosis'] = stats.kurtosis
-        features[objid]['min'] = stats.minmax[0]
-        features[objid]['max'] = stats.minmax[1]
-        features[objid]['amplitude'] = laobject.get_amplitude()[p]
-        features[objid]['skew'] = laobject.get_skew()[p]
-        features[objid]['somean'] = laobject.get_StdOverMean()[p]
-        features[objid]['shapiro'] = laobject.get_ShapiroWilk()[p]
-        features[objid]['q31'] = laobject.get_Q31()[p]
-        features[objid]['rms'] = laobject.get_RMS()[p]
-        features[objid]['mad'] = laobject.get_MAD()[p]
-        features[objid]['stetsonj'] = laobject.get_StetsonJ()[p]
-        features[objid]['stetsonk'] = laobject.get_StetsonK()[p]
-        features[objid]['acorr'] = laobject.get_AcorrIntegral()[p]
-        features[objid]['hlratio'] = laobject.get_hlratio()[p]
+        features['variance'] = stats.variance
+        features['skewness'] = stats.skewness
+        features['kurtosis'] = stats.kurtosis
+        features['min'] = stats.minmax[0]
+        features['max'] = stats.minmax[1]
+        features['amplitude'] = laobject.get_amplitude()[p]
+        features['skew'] = laobject.get_skew()[p]
+        features['somean'] = laobject.get_StdOverMean()[p]
+        features['shapiro'] = laobject.get_ShapiroWilk()[p]
+        features['q31'] = laobject.get_Q31()[p]
+        features['rms'] = laobject.get_RMS()[p]
+        features['mad'] = laobject.get_MAD()[p]
+        features['stetsonj'] = laobject.get_StetsonJ()[p]
+        features['stetsonk'] = laobject.get_StetsonK()[p]
+        features['acorr'] = laobject.get_AcorrIntegral()[p]
+        features['hlratio'] = laobject.get_hlratio()[p]
 
         if not table_exists:
             if redo:
                 database.exec_sql_query("TRUNCATE TABLE features;")  # Delete everything in the features table
-            mysql_fields = list(features[objid].keys())
+            mysql_fields = list(features.keys())
             mysql_formats = ['VARCHAR(255)', ] + ['FLOAT' for x in mysql_fields[1:]]
             mysql_schema = ', '.join(['{} {}'.format(x, y) for x, y in zip(mysql_fields, mysql_formats)])
-            table_name = database.create_sql_index_table_for_release(data_release, mysql_schema, redo=redo, table_name='features')
+            table_name = database.create_sql_index_table_for_release(data_release, mysql_schema, redo=redo, table_name='features_{}'.format(data_release))
             table_exists = True
 
-        features_out += [list(features[objid].values())]
+        features_out += [list(features.values())]
         nrows += 1
 
         # Save to mysql in batches of 1000
         if nrows >= 1000:
             features_out = np.array(features_out)
-            features_out = at.Table(features_out, names=list(features[objid].keys()))
+            features_out = at.Table(features_out, names=list(features.keys()))
             features_out = np.array(features_out).tolist()
             nsavedrows = database.write_rows_to_index_table(features_out, table_name)
             print("Saved ", nsavedrows, "rows")
@@ -128,19 +127,22 @@ def save_antares_features():
 
     if features_out:
         features_out = np.array(features_out)
-        features_out = at.Table(features_out, names=list(features[objid].keys()))
+        features_out = at.Table(features_out, names=list(features.keys()))
         features_out = np.array(features_out).tolist()
         nsavedrows = database.write_rows_to_index_table(features_out, table_name)
         print("Saved ", nsavedrows, "rows")
 
-    features = pd.DataFrame(features)
+    # features = pd.DataFrame(features)
 
-    print(features)
+    # print(features)
     return features
 
 
 if __name__ == '__main__':
-    save_antares_features()
-    plt.show()
+    # data_dir = os.path.join(ROOT_DIR, 'plasticc_data')
+    # for data_release in next(os.walk(data_dir))[DIRNAMES]:
+    #     if data_release == 'src':
+    #         continue
 
+    save_antares_features(data_release='20180221', redo=True)
 
