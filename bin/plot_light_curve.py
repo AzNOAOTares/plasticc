a41ef51671751503bb26d41a437969ce7c91f956
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2018-04-26 23:58:56 +0100

    Fixed bugs in plot_light_curve.py

--- bin/plot_light_curve.py
+++ bin/plot_light_curve.py
@@ -8,15 +8,27 @@ def plot_light_curve(objid, data_release='20180407'):
     field, model, base, snid = objid.split('_')
     result = getter.get_lcs_data(field=field, snid=snid, model=model, base=base)
     head, phot = next(result)
+    objid, ptrobs_min, ptrobs_max = head
     lc = getter.convert_pandas_lc_to_recarray_lc(phot)
 
-    for pb in ['u', 'r', 'i', 'g', 'z', 'Y']:
+    for pb in phot.keys():
         plt.errorbar(phot[pb]['MJD'], phot[pb]['FLUXCAL'], yerr=phot[pb]['FLUXCALERR'], fmt='.', label=pb)
     plt.legend()
 
+    plt.title(objid)
+    plt.xlabel('mjd')
+    plt.ylabel('flux')
+    plt.savefig('temp_light_curve')
+
 
 def main():
-    """ Input `python plot_light_curve.py objid data_release` """
+    """
+    Usage:
+        python plot_light_curve.py objid data_release
+
+    Example:
+         python -m bin.plot_light_curve WFD_80_%_%
+    """
 
     objid = str(sys.argv[1])
     if len(sys.argv) > 2:
@@ -24,8 +36,9 @@ def main():
     else:
         data_release = '20180407'
 
+    print(objid, data_release)
+
     plot_light_curve(objid, data_release)
-    plt.savefig('temp_light_curve')
     plt.show()
 
 
