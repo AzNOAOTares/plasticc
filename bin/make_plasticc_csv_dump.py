39cac59dd534883d9f8ff8a0e2331ae53d498d39
Gautham Narayan <gsnarayan@gmail.com>
2018-08-28 09:51:48 -0400

    gzipped output + fix missing specz to NaN + more comments + change file modification times

--- bin/make_plasticc_csv_dump.py
+++ bin/make_plasticc_csv_dump.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+# Person to blame: Gautham Narayan (gnarayan@stsci.edu)
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from collections import OrderedDict
@@ -23,6 +24,9 @@ import gc
 import warnings
 warnings.simplefilter('ignore')
 from astropy.time import Time
+import gzip
+
+# default - this is for tracking so we know what the last version that went to Kaggle was
 data_release = '20180827'
 
 
@@ -31,11 +35,13 @@ def task(entry):
     Worker function to process a table of light curves from fits_files
     """
     this_file_lcs = {}
+    # Specify exactly what columns to get from the photometry tables
     cols = ['MJD', 'FLT', 'FLUXCAL', 'FLUXCALERR', 'PHOTFLAG']
     # map the passbands to integers - include the extra space that's in Rick's files so we can skip stripping
     flt_map = {'u ':0,'g ':1,'r ':2, 'i ':3,'z ':4,'Y ':5}
     batch_key = entry[0]['object_id']
 
+    # get a list of unique FITS files for this batch of data and make sure the file exists
     fits_files = entry['filename']
     uniq_files = np.unique(fits_files)
     for fits_file in uniq_files:
@@ -43,7 +49,7 @@ def task(entry):
         if not os.path.exists(phot_file):
             phot_file = phot_file + '.gz'
 
-        # open this file 
+        # open this FITS file 
         try:
             phot_HDU = afits.open(phot_file, memmap=True)
         except Exception as e:
@@ -54,31 +60,39 @@ def task(entry):
         ind = entry['filename'] == fits_file
         object_ptrs = entry[ind]
     
+        # load all the requested objects from each FITS file into a dictionary
         for row in object_ptrs:
             objid = row['object_id']
             ptrobs_min = row['ptrobs_min']
             ptrobs_max = row['ptrobs_max']
+            # get the photometry for this object
             phot_data = data[ptrobs_min - 1:ptrobs_max]
 
+            # map the filter to an integer and convert data into a table
             lc = at.Table(phot_data)[cols]
             flt = np.array([flt_map[x] for x in lc['FLT']], dtype=np.uint8)
-            mjd = lc['MJD']
-            
-            thislc = at.Table([mjd, flt, lc['FLUXCAL'], lc['FLUXCALERR'], lc['PHOTFLAG']],\
+            thislc = at.Table([lc['MJD'], flt, lc['FLUXCAL'], lc['FLUXCALERR'], lc['PHOTFLAG']],\
                     names=['mjd','passband','flux','flux_err','photflag'])
+
+            # DO NOT OUTPUT SATURATED OBSERVATIONS - RK
             ind = thislc['photflag'] != 1024
             if len(thislc[ind]) == 0:
                 print(f'Fuck {objid} has no useful observations')
                 continue
             thislc = thislc[ind]
+
+            # map photflag to a bool 
             ind = thislc['photflag'] > 0
             thislc['photflag'][ind] = 1
+
+            # ensure the light curve is sorted by time
             thislc.sort('mjd')
             this_file_lcs[objid] = thislc 
         phot_HDU.close()
 
     batch_lines = []
     nbatch = 0
+    # preserving the requested order, convert each requested object into a list of CSV formatted lines
     for obj in entry['object_id']:
         lc = this_file_lcs.get(obj, None)
         if lc is not None:
@@ -88,7 +102,7 @@ def task(entry):
     return batch_key, nbatch, batch_lines
 
 
-def fixpath(filename, public=True):
+def fixpath(filename, public=True, gzip=False):
     dirn = os.path.dirname(filename)
     basen = os.path.basename(filename)
     if public:
@@ -99,9 +113,11 @@ def fixpath(filename, public=True):
         os.makedirs(dirn)
 
     fixn = os.path.join(dirn, basen)
+
+    if gzip:
+        if not fixn.endswith('.gz'):
+            fixn += '.gz'
     return fixn
-
-
 
 
 def main():
@@ -127,6 +143,7 @@ def main():
     offset = kwargs.pop('offset')
     limit  = kwargs.pop('limit')
 
+    # setup root filenames for output - these get changed by fixpath
     if dummy == 'training':
         outfile = os.path.join(dump_dir, 'plasticc_training_set.csv')
         offset = None
@@ -137,8 +154,10 @@ def main():
             if offset is None:
                 offset = 0
             outfile = os.path.join(dump_dir, 'plasticc_test_n{}_set.csv'.format(offset))
+            # if we're limiting the output, then just dump one file
+            nfiles = 1
 
-
+    # header file is named something sensible and is public
     header_file = outfile.replace('.csv','_metadata.csv')
     header_file = fixpath(header_file)
 
@@ -244,6 +263,7 @@ def main():
         target_map_data.write(target_map_file, format='ascii.fixed_width', delimiter=' ', overwrite=True)
         print(f'Wrote target mapping to file {target_map_file}')
 
+    # map the aggregated IDs to random target IDs
     target_map = dict(zip(train_types, target_types))
     target = np.array([target_map.get(x, 99) for x in out['sntype']])
     out['target'] = target
@@ -271,10 +291,11 @@ def main():
     
     # galactic objects have -9 as redshift - change to NaN
     # the numpy.isclose should have worked last time.... check this by hand.
-    dummy_val = np.repeat(-9, len(out))
     ind = out['hostgal_photoz'] == -9.
     out['hostgal_photoz'][ind] = np.nan
     out['hostgal_photoz_err'][ind] = np.nan
+    ind = out['hostgal_specz'] == -9.
+    out['hostgal_specz'][ind] = np.nan
 
     # figure out what fits files the data are in
     fits_files =[ "LSST_{0}_MODEL{1}/LSST_{0}_{2}_PHOT.FITS".format(*x.split('_')) for x in out['objid']] 
@@ -311,11 +332,13 @@ def main():
         # remove the model type from the output header that goes with the test data
         out.remove_column('target')
 
+        # make sure the truth table actually matches the job presently executing
         truth_file = outfile.replace('_set.csv', '_truthtable.csv')
         truth_file = fixpath(truth_file, public=False)
         if os.path.exists(truth_file):
             os.remove(truth_file)
-
+        
+        # write the truth table
         truth_table = at.Table([out_name, target], names=['object_id','target'])
         truth_table.write(truth_file)
         print(f'Wrote {truth_file}')
@@ -339,6 +362,7 @@ def main():
     # make batches to load the data 
     batches = []
     for ind in batch_inds:
+        # we need the fits file for each object + the object pointers
         this_batch_lcs = at.Table([out['object_id'][ind], 
                                     out['ptrobs_min'][ind], 
                                     out['ptrobs_max'][ind],
@@ -346,57 +370,112 @@ def main():
                                     names=['object_id','ptrobs_min', 'ptrobs_max', 'filename'])
         batches.append(this_batch_lcs)
     gc.collect()
+
+    #### HACK ####
+    # remove and rename some columns from the metadata output
+    # this isn't strictly necessary, since we choose exactly what columns to output
+    # but this makes sure astropy also strips any metadata about the columns itself
+    out.remove_columns(['objid', 'ptrobs_min', 'ptrobs_max', 'filename', 'sntype'])
+    out.rename_column('sim_dlmu','distance_modulus')
+   
+    # setup what columns get output into the headers
+    cols = ['object_id','ra','decl', 'ddf_bool', 'mwebv', 'mwebv_err', 'hostgal_specz', 'hostgal_photoz', 'hostgal_photoz_err', 'distance_modulus']
+    if dummy == 'training':
+        cols.append('target')
+    out = out[cols]
+
+    # write out the header
+    out.write(header_file, format='ascii.csv', overwrite=True)
+    #### HACK ####
+    sys.exit(-1)
+
+    # create a map from batch number to first objid in each batch
+    # batch number is helpful to name files by batch
+    # this is sequential, but you might imagine more complicated schemes
     batch_ids = np.arange(len(batches)) + 1
     batch_keys = [x['object_id'][0] for x in batches]
     batch_map = dict(zip(batch_keys, batch_ids))
 
+
     # do the output
     if dummy == 'training':
+        # training is simple -  dump each batch into one file in sequence
+        outfile = fixpath(outfile, gzip=True)
         with MultiPool() as pool:
             with tqdm(total=nmc) as pbar:
-                with open(outfile, 'w') as f:
-                    f.write('object_id,mjd,passband,flux,flux_err,detected_bool\n')
-                    # change to pool.imap so order is preserved in output file
-                    for result in pool.imap(task, batches):
-                        _, nbatch, batchlines = result
-                        pbar.update(nbatch)
-                        f.write('\n'.join(batchlines))
-                        f.write('\n')
+                outlines = 'object_id,mjd,passband,flux,flux_err,detected_bool\n'
+                # change to pool.imap so order is preserved in output file
+                # combine all the batches
+                for result in pool.imap(task, batches):
+                    _, nbatch, batchlines = result
+                    pbar.update(nbatch)
+                    outlines += '\n'.join(batchlines)
+                    outlines += '\n'
+                outbytes = outlines.encode()
                 gc.collect()
+
+                # do the output
+                with gzip.open(outfile, 'wb', compresslevel=9) as f:
+                    f.write(outbytes)
+
     else:
         with MultiPool() as pool:
+
+            # these variables will set the accessed time and modified time to the same numbers for all batches
+            st_atime = None 
+            st_mtime = None
+
             with tqdm(total=nmc) as pbar:
                 for batch in batches:
+                    # for test, the batches each get a separate file
                     batch_key = batch[0]['object_id']
                     batch_id = batch_map[batch_key]
                     batchfile = outfile.replace('.csv', f'_batch{batch_id}.csv')
-                    batchfile = fixpath(batchfile)
+                    batchfile = fixpath(batchfile, gzip=True)
+
+                    # for actual mutliprocessing, split up each file's indices into mini batches
                     ind = np.arange(len(batch))
                     mini_inds = np.array_split(ind, max(multiprocessing.cpu_count()-4, 0))
                     mini_batches = [batch[x] for x in mini_inds]
                     nbatch = 0
-                    batchlines = []
+
+                    # combine all the batches
+                    outlines = 'object_id,mjd,passband,flux,flux_err,detected_bool\n'
                     for result in pool.imap(task, mini_batches):
                         _, mini_nbatch, mini_batchlines = result
                         nbatch += mini_nbatch
-                        batchlines += mini_batchlines
+                        outlines += '\n'.join(mini_batchlines)
+                        outlines += '\n'
                         pbar.update(mini_nbatch)
-                    with open(batchfile, 'w') as f:
-                        f.write('object_id,mjd,passband,flux,flux_err,detected_bool\n')
-                        f.write('\n'.join(batchlines))
-                        f.write('\n')
+                    outbytes = outlines.encode()
                     gc.collect()
 
-    
-    # write out the header
+                    # do the output
+                    with gzip.open(batchfile, 'wb', compresslevel=9) as f:
+                        f.write(outbytes)
+
+                    # get the timestamps of the first batch file
+                    if st_atime is None:
+                       st = os.stat(batchfile)
+                       st_atime = st.st_atime
+                       st_mtime = st.st_mtime
+
+                    # change the timestamp of the output
+                    os.utime(batchfile, (st_atime, st_mtime))
+
+    # remove and rename some columns from the metadata output
+    # this isn't strictly necessary, since we choose exactly what columns to output
+    # but this makes sure astropy also strips any metadata about the columns itself
     out.remove_columns(['objid', 'ptrobs_min', 'ptrobs_max', 'filename', 'sntype'])
     out.rename_column('sim_dlmu','distance_modulus')
    
+    # setup what columns get output into the headers
     cols = ['object_id','ra','decl', 'ddf_bool', 'mwebv', 'mwebv_err', 'hostgal_specz', 'hostgal_photoz', 'hostgal_photoz_err', 'distance_modulus']
-
     if dummy == 'training':
         cols.append('target')
     out = out[cols]
+
+    # write out the header
     out.write(header_file, format='ascii.csv', overwrite=True)
 
 
