20e2522e4d8280d697d24ff80c508c34b0dd81dd
Gautham Narayan <gsnarayan@gmail.com>
2018-08-28 12:04:37 -0400

    fix upper limits

--- bin/make_extinction_comp_plot.py
+++ bin/make_extinction_comp_plot.py
@@ -44,48 +44,45 @@ def main():
     color = iter(cmap(np.linspace(0,1,nlines-3))) 
     fig1 = plt.figure(figsize=(15,10))
     ax1 = fig1.add_subplot(111)
+    if out_field == 'DDF':
+        upper_lim = 0.101
+        step = 0.001
+    else:
+        upper_lim = 0.81
+        step = 0.01
 
-    mwebv_range = np.arange(0, 1.01, 0.01)
+    mwebv_range = np.arange(0, upper_lim, step)
 
-    with PdfPages(f'{fig_dir}/rate_analysis/extinction_checks_{data_release}_{out_field}.pdf') as pdf:
-        for i,  model in enumerate(sntypes.keys()):
-            kwargs['model'] = model 
-            kwargs['big'] = True
-            head = getter.get_lcs_headers(**kwargs)
+    for i,  model in enumerate(sntypes.keys()):
+        kwargs['model'] = model 
+        kwargs['big'] = True
+        head = getter.get_lcs_headers(**kwargs)
+    
+        model_name = sntypes[model]
+        
+        head = list(head)
+        nobs = len(head) 
+        if nobs <= 1:
+            continue
+        
+        c = to_hex(next(color), keep_alpha=False)
+        objid, _, _, hz, dhz = zip(*list(head)) 
+        long_model_name = f'{model_name}_{model}'
+    
+        try:
+            density = gaussian_kde(hz, bw_method='scott')
+        except Exception as e:
+            continue 
+    
+        ax1.plot(mwebv_range, density(mwebv_range), color=c, label=long_model_name)
 
-            model_name = sntypes[model]
-            
-            head = list(head)
-            nobs = len(head) 
-            if nobs <= 1:
-                continue
-            
-            c = to_hex(next(color), keep_alpha=False)
-            objid, _, _, hz, dhz = zip(*list(head)) 
-            if nobs <= 2500:
-                g2 = (sns.jointplot(hz, dhz, color=c, kind='scatter', xlim=(0, 1), height=8).set_axis_labels("mwebv", "mwebv_err"))
-            else:
-                g2 = (sns.jointplot(hz, dhz, color=c, kind='hex', xlim=(0,1), height=8).set_axis_labels("z", "mwebv_err"))
-            fig2 = g2.fig
-            long_model_name = f'{model_name}_{model}'
-            fig2.suptitle(long_model_name)
-            fig2.tight_layout(rect=[0, 0, 1, 0.97])
-
-            try:
-                density = gaussian_kde(hz, bw_method='scott')
-            except Exception as e:
-                continue 
-
-            ax1.plot(mwebv_range, density(mwebv_range), color=c, label=long_model_name)
-
-            pdf.savefig(fig2)
-            plt.close(fig2)
-        ax1.set_xlabel('MWEBV', fontsize='xx-large')
-        ax1.set_ylabel('PDF', fontsize='xx-large')
-        ax1.legend(frameon=False)
-        ax1.set_xlim(0, 1)
-        pdf.savefig(fig1)
-        plt.close(fig1)
+    ax1.set_xlabel('MWEBV', fontsize='xx-large')
+    ax1.set_ylabel('PDF', fontsize='xx-large')
+    ax1.legend(frameon=False)
+    ax1.set_xlim(0, upper_lim - step)
+    fig1.tight_layout()
+    fig1.savefig(f'{fig_dir}/rate_analysis/extinction_checks_{data_release}_{out_field}.pdf')
+    plt.close(fig1)
 
 
 if __name__=='__main__':
